5baec775fcb3915a97d3d3b54bbe23cf5675bb96
Abdul <abdulshafey24@gmail.com>
2024-02-11 14:46:41 +0530

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("db connecred..."))
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.listen(3002, () => {
   console.log("Server running on 3002...");
