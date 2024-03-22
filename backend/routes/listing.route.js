214caca2043a499e594a8b0462652c5e2b175c4b
Abdul <abdulshafey24@gmail.com>
2024-03-23 03:51:45 +0530

    create search api route

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   handleCreateListing,
   handleDeleteListing,
   handleGetListing,
+  handleGetListings,
   handleUpdateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -13,4 +14,5 @@ router.post("/create", verifyToken, handleCreateListing);
 router.delete("/delete/:id", verifyToken, handleDeleteListing);
 router.post("/update/:id", verifyToken, handleUpdateListing);
 router.get("/get/:id", handleGetListing);
+router.get("/get", handleGetListings);
 export default router;
