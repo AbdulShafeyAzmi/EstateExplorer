c859932f9a3d421651ebce81efe40b063bbdc0b3
Abdul <abdulshafey24@gmail.com>
2024-03-28 03:13:25 +0530

    Create the listing item component and show listings

--- frontend/src/pages/UpdateListing.jsx
+++ frontend/src/pages/UpdateListing.jsx
@@ -376,7 +376,7 @@ export default function UpdateListing() {
             disabled={loading || uploading}
             className="p-3 bg-slate-700 text-white rounded-lg hover:opacity-95 disabled:opacity-80 uppercase"
           >
-            {loading ? "Creating..." : "Update Listing"}
+            {loading ? "Updating..." : "Update Listing"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
